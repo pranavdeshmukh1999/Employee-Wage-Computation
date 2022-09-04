7a3960c403659e1d97e27aeb67aade85bf71da59
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-04 13:52:22 +0530

    Added:Perday-salary logic

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -6,17 +6,25 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 
-		 System.out.println("Welcome to Employee Wage Computation");
-		 
-		 Random random = new Random ();
-	
-	int randomNum = random.nextInt(2);
-	
-	   if(randomNum == 1)
-			   System.out.println("Employee is present");
-	   
-	   else
-		   System.out.println("Employee is absent");
+		System.out.println("Welcome to Employee Wage Computation");
+
+		int wagePerHour = 20;
+		int fullDayHours=8;
+		int salary=0;
+		
+		Random random = new Random();
+
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+		{
+			System.out.println("Employee is present");
+			salary = wagePerHour * fullDayHours;
+		}
+		else
+			System.out.println("Employee is absent");
+		
+		System.out.println("Employee salary :"+salary);
 	}
 
 }
